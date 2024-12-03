bfe5c61cf5245598c62cb54c77adddd260bb97d4
Grisha <guruwolf8@gmail.com>
2024-12-03 17:33:39 +0300

    Создание пакета dto и класса Employee
    Creating a dto package and an Employee class

--- src/main/java/com/GrishaUp/JavaCoreSkyPro/controller/CheckedController.java
+++ src/main/java/com/GrishaUp/JavaCoreSkyPro/controller/CheckedController.java
@@ -1,12 +1,15 @@
 package com.GrishaUp.JavaCoreSkyPro.controller;
 
+import com.GrishaUp.JavaCoreSkyPro.dtoEmployee.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CheckedController {
     @GetMapping
-    public String helloWorld() {
-        return "HelloWorld";
+    public String getEmployee() {
+        Employee employee = new Employee("Вася", "Пупкин");
+        return employee.toString();
     }
 }
