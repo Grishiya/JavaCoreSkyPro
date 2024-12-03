d9a03b0f135c83e575222abf9e8d2430f2367af1
Grisha <guruwolf8@gmail.com>
2024-12-03 16:19:12 +0300

    Первый запуск, проверка работы спринга. The first launch, checking the operation of the spring.

---
+++ src/main/java/com/GrishaUp/JavaCoreSkyPro/JavaCoreSkyProApplication.java
@@ -0,0 +1,13 @@
+package com.GrishaUp.JavaCoreSkyPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreSkyProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoreSkyProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GrishaUp/JavaCoreSkyPro/JavaCoreSkyProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GrishaUp.JavaCoreSkyPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoreSkyProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
